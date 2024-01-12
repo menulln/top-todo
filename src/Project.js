ae4e642af1ad5f2460d3b809c86d4c77527af177
menulln <39300523+menulln@users.noreply.github.com>
2024-01-12 21:23:51 +0100

    Add addTask() and deleteTask() methods

--- src/Project.js
+++ src/Project.js
@@ -3,4 +3,16 @@ export default class Project {
         this.name = name;
         this.tasks = [];
     }
+
+    addTask(task) {
+        this.tasks.push(task);
+    }
+
+    deleteTask(taskTitle) {
+        this.tasks.forEach((task) => {
+            if (task.title === taskTitle) {
+                return this.tasks.splice(this.tasks.indexOf(task.title) - 1, 1);
+            }
+        });
+    }
 }
