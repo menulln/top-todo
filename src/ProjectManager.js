9ab6fef2c2af4c1279072e3909b3a6efcc574929
menulln <39300523+menulln@users.noreply.github.com>
2024-01-13 20:32:10 +0100

    Add addProject and deleteProject methods

--- src/ProjectManager.js
+++ src/ProjectManager.js
@@ -1,3 +1,16 @@
 export default class ProjectManager {
+
     static projects = [];
+
+    static addProject(project) {
+        this.projects.push(project);
+    }
+
+    static deleteProject(projectTitle) {
+        this.projects.forEach((project) => {
+            if (project.title === projectTitle) {
+                return this.projects.splice(this.projects.indexOf(project.title) - 1, 1);
+            }
+        });
+    }
 }
