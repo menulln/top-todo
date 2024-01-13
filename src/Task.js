96fc9db54ead9c1a6a013ccf05419e5cac8c0ff9
menulln <39300523+menulln@users.noreply.github.com>
2024-01-13 20:25:44 +0100

    Add setters and getters

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,45 @@
 import { formatDistanceToNow } from "date-fns";
 
 export default class Task {
-    constructor(title, description, dueDate, priority) {
+    constructor(title, description, priority, dueDate) {
         this.title = title;
         this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.timeLeft = formatDistanceToNow(dueDate);
+        this._priority = priority;
+        this._dueDate = dueDate;
+        this._timeLeft = formatDistanceToNow(dueDate);
+    }
+    
+    get priority() {
+        return this._priority;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    get timeLeft() {
+        return this._timeLeft;
+    }
+
+    set priority(priority) {
+        const priorities = ['high', 'medium', 'low'];
+        if (priorities.includes(priority)) {
+            this._priority = priority;
+        } else {
+            throw 'Error: No valid priority';
+        }
+    }
+
+    set dueDate(date) {
+        if (date instanceof Date) {
+            this._dueDate = date;
+            this._timeLeft = formatDistanceToNow(date);
+        } else {
+            throw 'Error: No valid Date object';
+        }
+    }
+
+    set timeLeft(value) {
+        throw 'Error: Can\'t change value of timeLeft';
     }
 }
