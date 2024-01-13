9c62655c387de7c2d4b0033fe5d03066508a60ee
menulln <39300523+menulln@users.noreply.github.com>
2024-01-13 20:25:22 +0100

    Add timeLeft property

--- src/Task.js
+++ src/Task.js
@@ -1,8 +1,11 @@
+import { formatDistanceToNow } from "date-fns";
+
 export default class Task {
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.timeLeft = formatDistanceToNow(dueDate);
     }
 }
