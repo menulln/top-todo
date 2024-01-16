04c6300eb342bb70268c28173cdd5d2a57757a33
menulln <39300523+menulln@users.noreply.github.com>
2024-01-16 12:46:35 +0100

    Add renderProjects function

--- src/modules/Display.js
+++ src/modules/Display.js
@@ -6,4 +6,52 @@ export default class Display {
         document.body.appendChild(nav);
         document.body.appendChild(main);
     }
+
+    static renderProjects(projects) {
+        if (document.querySelector('.projects')) {
+            const ul = document.querySelector('.projects');
+            ul.remove();
+        }
+    
+        const nav = document.querySelector('nav');
+        const ul = document.createElement('ul');
+    
+        ul.classList.toggle('projects');
+    
+        projects.forEach((project) => {
+            const li = document.createElement('li');
+            const projectTitle = document.createElement('p');
+            const closeIcon = document.createElement('i');
+    
+            projectTitle.textContent = project.title;
+            closeIcon.textContent = '\u{00D7}';
+    
+            projectTitle.classList.toggle('project-title');
+            closeIcon.classList.toggle('close-icon');
+    
+            projectTitle.addEventListener('click', () => {
+                this.renderProject(project);
+                ProjectManager.currentProject = project;
+            });
+    
+            li.appendChild(projectTitle);
+            li.appendChild(closeIcon);
+            ul.appendChild(li);
+        });
+    
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.textContent = '+';
+    
+        p.addEventListener('click', () => {
+            console.log(ProjectManager.projects);
+            const test = new Project('test');
+            ProjectManager.addProject(test);
+            Display.renderProjects(ProjectManager.projects);
+        }); 
+    
+        li.appendChild(p);
+        ul.appendChild(li);
+        nav.appendChild(ul);
+    }
 }
