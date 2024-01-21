b49d6fe3b480376d3ac4cae62fd3e4d7b4cf22e0
menulln <39300523+menulln@users.noreply.github.com>
2024-01-21 16:00:44 +0100

    Add saveProjects method

--- src/modules/ProjectManager.js
+++ src/modules/ProjectManager.js
@@ -1,10 +1,15 @@
+import Project from "./Project";
+import Task from "./Task";
+import LocalStorage from "./LocalStorage";
+
 export default class ProjectManager {
 
     static currentProject;
     static projects = [];
 
     static addProject(project) {
-        this.projects.push(project);
+        this.projects.push(project);   
+        this.saveProjects();
     }
 
     static deleteProject(projectTitle) {
@@ -13,5 +18,11 @@ export default class ProjectManager {
                 return this.projects.splice(this.projects.findIndex((project) => project.title === projectTitle), 1);
             }
         });
+        this.saveProjects();
+    }
+
+    static saveProjects() {
+        LocalStorage.clear();
+        LocalStorage.setItem('projects', JSON.stringify(ProjectManager.projects));
     }
 }
